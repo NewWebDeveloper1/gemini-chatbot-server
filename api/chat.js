4ba3ffa2d620ccc2fe67632ba2c6992536da2b9e
Nitish Kumar <lenitishk@gmail.com>
2025-05-18 23:05:49 +0530

    add url to  allowed Origins

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,7 @@
-const allowedOrigins = ["http://127.0.0.1:5500"];
+const allowedOrigins = [
+  "http://127.0.0.1:5500",
+  "https://gemini-chatbot-replica.vercel.app",
+];
 
 const handler = async (req, res) => {
   const origin = req.headers.origin;
